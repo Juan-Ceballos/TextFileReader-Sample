67bd658665137ae7e4eca45fc55c22a7803ea1b9
Juan-Ceballos <juanceballos@pursuit.org>
2024-01-11 23:21:35 -0500

    fc

---
+++ TextFileReader-Sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TextFileReader-Sample
+//
+//  Created by Juan ceballos on 1/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var fileTextLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        fileTextLabel.text = "Hello"
+    }
+    
+    
+    
+
+
+}
+
